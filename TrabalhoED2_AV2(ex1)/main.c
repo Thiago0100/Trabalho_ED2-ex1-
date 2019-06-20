481f67de775176dcd6facb06fb4294a25cde2c1a
Thiago0100 <38388023+Thiago0100@users.noreply.github.com>
2019-06-20 13:39:46 -0300

    incluindo método raiz quadrada

--- TrabalhoED2_AV2(ex1)/main.c
+++ TrabalhoED2_AV2(ex1)/main.c
@@ -1,8 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+
+float raizQuadrada(int valor)
 {
-    printf("Hello world!\n");
-    return 0;
+    int minimo = 0, i;
+    int* vetor;
+    //se a o valor da raiz quadrada for menor que zero,
+    //então retorne zero.
+    if(valor <= minimo)
+    {
+        return 0;
+    }
+
+    //(valor + 1), necessário para poder inclui o próprio valor e o zero.
+    vetor = (int*) malloc((valor + 1) * sizeof(int));
+
+    if(vetor == NULL)
+    {
+        printf("\n>>>> Memória insuficiente! <<<<\n");
+    }
+
+    for(i = 0; i <= valor + 1; i++)
+    {
+        vetor[i] = i;
+    }
+
+    /*   Testa vetor
+    for(i = 0; i <= valor; i++)
+    {
+        printf("posicao %i: %d\n\n", i, vetor[i]);
+    }*/
+
+
+
+}
+
+
+void main()
+{
+    int numero = 10;
+    printf("Raiz quadrada: %f\n", raizQuadrada(numero));
+
 }
