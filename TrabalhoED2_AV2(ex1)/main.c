8da395457dce5213814422a14af57fe2effa4588
Thiago0100 <38388023+Thiago0100@users.noreply.github.com>
2019-06-20 13:50:32 -0300

    .

--- TrabalhoED2_AV2(ex1)/main.c
+++ TrabalhoED2_AV2(ex1)/main.c
@@ -1,36 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct item
+{
+    int* vetor;
+    int numero;
 
-float raizQuadrada(int valor)
+}TipoItem;
+
+typedef struct lista
+{
+    int* primeiro;
+    int* ultimo;
+
+}TipoLista;
+
+
+/*
+int BuscaBinariaLista(TipoLista *lista, TipoItem item)
+{
+    int inf = 0, sup = lista->ultimo, busca = -1, meio;
+
+    while (inf <= sup)
+    {
+        meio = (inf + sup) / 2;
+
+        if (lista->itens[meio].chave == item.chave)
+        {
+            busca = meio; // índice do nó encontrado
+            inf = sup + 1;
+        }
+
+        else if (lista->itens[meio].chave < item.chave)
+            inf = meio + 1;
+
+        else
+            sup = meio - 1;
+    }
+
+    return busca;
+}
+*/
+
+float raizQuadrada(TipoItem item)
 {
     int minimo = 0, i;
-    int* vetor;
+
     //se a o valor da raiz quadrada for menor que zero,
     //então retorne zero.
-    if(valor <= minimo)
+    if(item.numero <= minimo)
     {
         return 0;
     }
 
     //(valor + 1), necessário para poder inclui o próprio valor e o zero.
-    vetor = (int*) malloc((valor + 1) * sizeof(int));
+    item.vetor = (int*) malloc((item.numero + 1) * sizeof(int));
 
-    if(vetor == NULL)
+    if(item.vetor == NULL)
     {
         printf("\n>>>> Memória insuficiente! <<<<\n");
     }
 
-    for(i = 0; i <= valor + 1; i++)
+    for(i = 0; i <= item.numero + 1; i++)
     {
-        vetor[i] = i;
+        item.vetor[i] = i;
     }
 
-    /*   Testa vetor
-    for(i = 0; i <= valor; i++)
+       //Testa vetor
+    for(i = 0; i <= item.numero; i++)
     {
-        printf("posicao %i: %d\n\n", i, vetor[i]);
-    }*/
+        printf("posicao %i: %d\n\n", i, item.vetor[i]);
+    }
 
 
 
@@ -39,7 +79,9 @@ float raizQuadrada(int valor)
 
 void main()
 {
-    int numero = 10;
-    printf("Raiz quadrada: %f\n", raizQuadrada(numero));
+    TipoItem item;
+
+    item.numero = 10;
+    printf("Raiz quadrada: %f\n", raizQuadrada(item));
 
 }
