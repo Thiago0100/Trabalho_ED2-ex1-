c71142972fdc9a321016eeabe7b6ee529bb55edd
Thiago0100 <38388023+Thiago0100@users.noreply.github.com>
2019-06-21 00:53:21 -0300

    finalizando o projeto 1

--- TrabalhoED2_AV2(ex1)/main.c
+++ TrabalhoED2_AV2(ex1)/main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define VALOR_RAIZ_QUADRADA 121
+#define CASAS_DECIMAIS 100  // são as quantidades de posicoes em relação a quantidade de casas decimais.
 
 typedef struct item
 {
-    int* lista;    //vetor
+    int* lista1;    //vetor1
+    float* lista2;    //vetor2
     int chave;  // VALOR_RAIZ_QUADRADA
     float resultado_raiz_quadrada;
+    int tipo_de_lista;
 
 }TipoItem;
 
@@ -18,52 +20,156 @@ typedef struct lista
 }TipoLista;
 
 
-int buscaBinariaLista(TipoItem item);
-int raizQuadrada(TipoItem item);
+int buscaBinariaLista(TipoItem* item);
+int raizQuadrada(TipoItem* item);
 
 
 void main()
 {
+
     TipoItem item;
-    int posicao;
+    int posicao, valor, condicao_de_parada = 1;
 
-    item.chave = VALOR_RAIZ_QUADRADA;
-    item.resultado_raiz_quadrada = raizQuadrada(item);
-
-    printf("\n\n>>> Raiz quadrada de %d eh: %f\n\n\n", VALOR_RAIZ_QUADRADA, item.resultado_raiz_quadrada);
-
-    system("pause");
+    do
+    {
+        printf("\n\n\nInsira um numero para obter a raiz quadrada: ");
+        scanf("%d", &valor);
 
 
+        item.tipo_de_lista = 1;
+        item.chave = valor;
+        posicao = raizQuadrada(&item);
+
+        if(item.tipo_de_lista == 1)
+        {
+            printf("\n\n>>> Raiz quadrada de %d eh: %d\n\n\n", valor, item.lista1[posicao]);
+        }
+
+        else
+        {
+            printf("\n\n>>> Raiz quadrada de %d eh: %.2f\n\n\n", item.chave, item.lista2[posicao]);
+        }
+
+        printf("\n\nDeseja continuar? Digite 0 para sair.\n\n");
+        scanf("%d", &condicao_de_parada);
+
+    }while(condicao_de_parada != 0);
 }
 
 
-int buscaBinariaLista(TipoItem item)
+int buscaBinariaLista(TipoItem* item)
 {
-    int inf = 0, sup = VALOR_RAIZ_QUADRADA, busca = -1, meio;
+    int inf = 0, sup = item->chave, busca = -1, meio;
+    int valor_proximo1, i;
+    float auxiliar = 0.00, valor_proximo2;
 
-    while (inf <= sup)
+    if(item->tipo_de_lista == 2)
     {
-        meio = (inf + sup) / 2;
+        sup = CASAS_DECIMAIS;
 
-        if ((item.lista[meio]*item.lista[meio]) == item.chave)
+        while (inf <= sup)
         {
-            busca = meio; // índice do nó encontrado
-            inf = sup + 1;
+            meio = (inf + sup) / 2;
+
+            if ((item->lista2[meio]*item->lista2[meio]) == item->chave)
+            {
+                busca = meio; // índice do nó encontrado
+                inf = sup + 1;
+            }
+
+            else if ((item->lista2[meio]*item->lista2[meio]) < item->chave)
+            {
+                inf = meio + 1;
+                valor_proximo2 = item->lista2[meio];
+            }
+
+            else
+            {
+                sup = meio - 1;
+                valor_proximo2 = item->lista2[meio];
+            }
         }
 
-        else if ((item.lista[meio]*item.lista[meio]) < item.chave)
-            inf = meio + 1;
-
-        else
-            sup = meio - 1;
+        busca = meio - 1;
     }
 
-    //se não encontrar com valores inteiros ele irá buscar com números decimais.
-    if(busca == -1)
+    if(item->tipo_de_lista == 1)
     {
+        while (inf <= sup)
+        {
+            meio = (inf + sup) / 2;
 
+            if ((item->lista1[meio]*item->lista1[meio]) == item->chave)
+            {
+                busca = meio; // índice do nó encontrado
+                inf = sup + 1;
+            }
 
+            else if ((item->lista1[meio]*item->lista1[meio]) < item->chave)
+            {
+                inf = meio + 1;
+                valor_proximo1 = item->lista1[meio];
+            }
+
+            else
+            {
+                sup = meio - 1;
+                valor_proximo1 = item->lista1[meio];
+            }
+        }
+
+        //se não encontrar com valores inteiros ele irá buscar com números decimais.
+        if(busca == -1)
+        {
+            if(valor_proximo1 * valor_proximo1 > item->chave)
+            {
+                valor_proximo1--;
+            }
+
+            item->lista2 = (float*) malloc(CASAS_DECIMAIS * sizeof(float));
+
+            if(item->lista2 == NULL)
+            {
+                printf("\n>>>> Memoria insuficiente! <<<<\n");
+                system("pause");
+            }
+
+            for(i = 0; i < CASAS_DECIMAIS; i++)
+            {
+                auxiliar += 0.01;
+                item->lista2[i] = valor_proximo1 + auxiliar;
+            }
+
+            item->tipo_de_lista = 2;
+
+            busca = buscaBinariaLista(item);
+        }
+
+        else
+        {
+            while (inf <= sup)
+            {
+                meio = (inf + sup) / 2;
+
+                if ((item->lista2[meio]*item->lista2[meio]) == item->chave)
+                {
+                    busca = meio; // índice do nó encontrado
+                    inf = sup + 1;
+                }
+
+                else if ((item->lista2[meio]*item->lista2[meio]) < item->chave)
+                {
+                    inf = meio + 1;
+                    valor_proximo1 = item->lista2[meio];
+                }
+
+                else
+                {
+                    sup = meio - 1;
+                    valor_proximo1 = item->lista2[meio];
+                }
+            }
+        }
     }
 
     //retorna o indice
@@ -71,37 +177,30 @@ int buscaBinariaLista(TipoItem item)
 }
 
 
-int raizQuadrada(TipoItem item)
+int raizQuadrada(TipoItem *item)
 {
     int minimo = 0, i, posicao;
 
     //se a o valor da raiz quadrada for menor que zero,
     //então retorne zero.
-    if(item.chave <= minimo)
+    if(item->chave <= minimo)
     {
         return 0;
     }
 
     //(valor + 1), necessário para poder inclui o próprio valor e o zero.
-    item.lista = (int*) malloc((item.chave + 1) * sizeof(int));
+    item->lista1 = (int*) malloc((item->chave + 1) * sizeof(int));
 
-    if(item.lista == NULL)
+    if(item->lista1 == NULL)
     {
         printf("\n>>>> Memoria insuficiente! <<<<\n");
         system("pause");
     }
 
-    for(i = 0; i <= item.chave; i++)
+    for(i = 0; i <= item->chave; i++)
     {
-        item.lista[i] = i;
+        item->lista1[i] = i;
     }
-
-       //Testa vetor
-    for(i = 0; i <= item.chave; i++)
-    {
-        printf("posicao %d: %d\n\n", i, item.lista[i]);
-    }
-
 
     return buscaBinariaLista(item);
 }
